b0addeb121c4da1e0c9fe3c3fbe4a3279b05dd25
Tuchanski <gui.rocha2004@gmail.com>
2024-06-23 20:54:03 -0300

    Implementa métodos de adicionar elemento, aumentar capacidade e remover elemento do vetor

--- vetores-arrays/src/testes/Teste06.java
+++ vetores-arrays/src/testes/Teste06.java
@@ -2,22 +2,28 @@ package testes;
 
 import vetor.Vetor;
 
-public class Teste05 {
+public class Teste06 {
 
     public static void main(String[] args) {
+
         try {
 
-            Vetor vetor = new Vetor(7);
+            Vetor vetor = new Vetor(10);
 
             vetor.adicionarElemento(1);
             vetor.adicionarElemento(2);
             vetor.adicionarElemento(3);
+            vetor.adicionarElemento(4);
 
-            System.out.println(vetor.buscarPorElemento(9));
+            vetor.adicionarElementoEmPosicao(7, 3);
+
+            System.out.println(vetor);
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+
+        
     }
 
 }
--- vetores-arrays/src/vetor/Vetor.java
+++ vetores-arrays/src/vetor/Vetor.java
@@ -10,11 +10,23 @@ public class Vetor {
         this.tamanhoReal = 0;
     }
 
+    private void aumentarCapacidade() {
+
+        if (this.tamanhoReal == this.elementos.length) {
+            Integer[] elementosNovos = new Integer[this.elementos.length * 2];
+            for (int i = 0; i < this.elementos.length; i++) {
+                elementosNovos[i] = this.elementos[i];
+            }
+            this.elementos = elementosNovos;
+        }
+
+    }
+
     // Por questões de eficiência, não é interessante iterar sobre cada elemento e
     // verificar se está NULL.
     // Portanto, utiliza-se uma variável para guardar o tamanho real do vetor.
     public void adicionarElemento(int elemento) throws Exception {
-
+        aumentarCapacidade();
         if (this.tamanhoReal < this.elementos.length) {
             this.elementos[this.tamanhoReal] = elemento;
             // System.out.printf("Elemento %d inserido com sucesso.\n", this.tamanhoReal);
@@ -35,9 +47,9 @@ public class Vetor {
 
     }
 
-    public int buscaPorElemento(int elemento) {
+    public int buscarPorElemento(int elemento) {
 
-        // Busca sequêncial
+        // Busca sequencial
         for (int i = 0; i < this.tamanhoReal; i++) {
             if (this.elementos[i] == elemento) {
                 return i;
@@ -45,6 +57,30 @@ public class Vetor {
         }
         return -1; // Posição inexistente
 
+    }
+
+    public void adicionarElementoEmPosicao(int elemento, int posicao) {
+        aumentarCapacidade();
+        if (!(posicao >= 0 && posicao < this.tamanhoReal)) {
+            throw new IllegalArgumentException("Erro: Índice inexistente.");
+        }
+
+        for (int i = this.tamanhoReal - 1; i >= posicao; i--) {
+            this.elementos[i + 1] = this.elementos[i];
+        }
+        this.elementos[posicao] = elemento;
+        this.tamanhoReal += 1;
+
+    }
+
+    public void removerElementoPorPosicao(int posicao){
+        if (!(posicao >= 0 && posicao < this.tamanhoReal)) {
+            throw new IllegalArgumentException("Erro: Índice inexistente.");
+        }
+        for (int i = posicao; i < this.tamanhoReal - 1; i++){
+            this.elementos[i] = this.elementos[i + 1];
+        }
+        this.tamanhoReal -= 1;
     }
 
     public int getTamanhoReal() {
