ed541d6f76b95a00d054015670c0078509dea082
Tuchanski <gui.rocha2004@gmail.com>
2024-06-23 21:11:52 -0300

    Implementa generalização do vetor

---
+++ vetores-arrays/src/testes/Teste09.java
@@ -0,0 +1,29 @@
+package testes;
+
+import vetor.VetorObject;
+
+public class Teste09 {
+
+    public static void main(String[] args) {
+
+        VetorObject vetor = new VetorObject(3);
+
+        Usuario user1 = new Usuario("João Afonso", "coxa@gmail.com", "123-456-789");
+        Usuario user2 = new Usuario("Pitoco Mendes", "pitoco@gmail.com", "765-432-123");
+        Usuario user3 = new Usuario("Princeso Afonso", "afonsinho@gmail.com", "654-678-432");
+
+        try {
+
+            vetor.adicionarElemento(user1);
+            vetor.adicionarElemento(user2);
+            vetor.adicionarElemento(user3);
+
+            System.out.println(vetor);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
