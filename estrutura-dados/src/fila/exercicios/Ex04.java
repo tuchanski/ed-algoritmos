b5069281c872a7d6e343a48f9a17e93f4f193a42
Tuchanski <gui.rocha2004@gmail.com>
2024-06-25 17:36:17 -0300

    Adiciona exercício 4 de fila

--- estrutura-dados/src/fila/exercicios/Ex04.java
+++ estrutura-dados/src/fila/exercicios/Ex04.java
@@ -1,9 +1,47 @@
 package fila.exercicios;
 
+import java.util.Random;
+
+import fila.models.Fila;
+
 public class Ex04 {
 
     public static void main(String[] args) {
-        
+
+        // Escreva um programa em filas que simule a brincadeira da batata-quente
+
+        Fila<Integer> fila = new Fila<>();
+
+        try {
+
+            for (int i = 0; i < 11; i++){
+                fila.enfileiraElemento(i);
+            }
+
+            Random aleatorio = new Random();
+
+            int num = 0;
+
+            while (num == 0){
+                num = aleatorio.nextInt(10);
+            }
+
+            System.out.println("Numero: " + num);
+
+            while (fila.getTamanhoReal() > 1){
+                for (int j = 0; j < num; j++){
+                    fila.enfileiraElemento(fila.desenfileirarElemento());
+                }
+                System.out.println("Eliminado: " + fila.desenfileirarElemento());
+            }
+
+            System.out.println("Ganhador: " + fila.desenfileirarElemento());
+
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
-    
+
 }
